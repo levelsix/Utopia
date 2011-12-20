e6cf5a819bf278858e2d9a5d2e3a539e78ca4f4d
Conrad <conrad@lvl6.com>
2011-12-19 19:34:18 -0800

    added EventProtocol.java

---
+++ src/com/lvl6/utils/EventProtocol.java
@@ -0,0 +1,31 @@
+package com.lvl6.utils;
+
+public class EventProtocol {
+
+   public static final byte C_CHAT_EVENT = (byte) 100;
+   public static final byte C_TRADE_EVENT = (byte) 101;
+   public static final byte C_PVP_EVENT = (byte) 102;
+   public static final byte C_MISSION_EVENT = (byte) 103;
+   public static final byte C_REALESTATE_EVENT = (byte) 104;
+   public static final byte C_ITEM_EVENT = (byte) 105;
+   public static final byte C_INAPPP_EVENT = (byte) 106;
+   public static final byte C_RECRUIT_EVENT = (byte) 107;
+   public static final byte C_HEALER_EVENT = (byte) 108;
+   public static final byte C_BANK_EVENT = (byte) 109;
+   public static final byte C_VIEWMESSAGES_EVENT = (byte) 110;
+   public static final byte C_PLACEBUILDING_EVENT = (byte) 111;
+   
+   public static final byte S_CHAT_EVENT = (byte) 112;
+   public static final byte S_TRADE_EVENT = (byte) 113;
+   public static final byte S_PVP_EVENT = (byte) 114;
+   public static final byte S_MISSION_EVENT = (byte) 115;
+   public static final byte S_REALESTATE_EVENT = (byte) 116;
+   public static final byte S_ITEM_EVENT = (byte) 117;
+   public static final byte S_INAPPP_EVENT = (byte) 118;
+   public static final byte S_RECRUIT_EVENT = (byte) 119;
+   public static final byte S_HEALER_EVENT = (byte) 120;
+   public static final byte S_BANK_EVENT = (byte) 121;
+   public static final byte S_VIEWMESSAGES_EVENT = (byte) 122;
+   public static final byte S_PLACEBUILDING_EVENT = (byte) 123;
+   
+}
