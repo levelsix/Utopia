6b0a1eb224cd122f72a53aa7ec4e0f1d64077ef2
Ashwin Kamath <ashwin@lvl6.com>
2012-04-08 08:30:38 -0700

    new iap packages

--- src/com/lvl6/properties/IAPValues.java
+++ src/com/lvl6/properties/IAPValues.java
@@ -17,32 +17,29 @@ public class IAPValues {
   public static final String APP_ITEM_ID = "app_item_id";
   public static final String PURCHASE_DATE = "purchase_date";
   
-  private static final String PACKAGE1 = "com.lvl6.utopia.fewdiamonds";
-  private static final String PACKAGE2 = "com.lvl6.utopia.morediamonds";
-  private static final String PACKAGE3 = "com.lvl6.utopia.quitesomediamonds";
-  private static final String PACKAGE4 = "com.lvl6.utopia.rackdiamonds";
-  private static final String PACKAGE5 = "com.lvl6.utopia.shittondiamonds";
-  private static final String PACKAGE6 = "com.lvl6.utopia.toomanydiamonds";
+  private static final String PACKAGE1 = "com.lvl6.lostnations.package1";
+  private static final String PACKAGE2 = "com.lvl6.lostnations.package2";
+  private static final String PACKAGE3 = "com.lvl6.lostnations.package3";
+  private static final String PACKAGE4 = "com.lvl6.lostnations.package4";
+  private static final String PACKAGE5 = "com.lvl6.lostnations.package5";
   
   private static final int PACKAGE_1_DIAMONDS = 50;
   private static final int PACKAGE_2_DIAMONDS = 100;
-  private static final int PACKAGE_3_DIAMONDS = 250;
+  private static final int PACKAGE_3_DIAMONDS = 200;
   private static final int PACKAGE_4_DIAMONDS = 500;
   private static final int PACKAGE_5_DIAMONDS = 1000;
-  private static final int PACKAGE_6_DIAMONDS = 2000;
   
-  private static final double PACKAGE_1_PRICE = .99;
-  private static final double PACKAGE_2_PRICE = 4.99;
-  private static final double PACKAGE_3_PRICE = 9.99;
-  private static final double PACKAGE_4_PRICE = 19.99;
+  private static final double PACKAGE_1_PRICE = 4.99;
+  private static final double PACKAGE_2_PRICE = 9.99;
+  private static final double PACKAGE_3_PRICE = 19.99;
+  private static final double PACKAGE_4_PRICE = 49.99;
   private static final double PACKAGE_5_PRICE = 99.99;
-  private static final double PACKAGE_6_PRICE = 999.99;
 
   public static final List<String> packageNames = 
-      Arrays.asList(PACKAGE1, PACKAGE2, PACKAGE3, PACKAGE4, PACKAGE5, PACKAGE6);
+      Arrays.asList(PACKAGE1, PACKAGE2, PACKAGE3, PACKAGE4, PACKAGE5);
   public static final List<Integer> packageGivenDiamonds = 
       Arrays.asList(PACKAGE_1_DIAMONDS, PACKAGE_2_DIAMONDS, PACKAGE_3_DIAMONDS, 
-          PACKAGE_4_DIAMONDS, PACKAGE_5_DIAMONDS, PACKAGE_6_DIAMONDS);
+          PACKAGE_4_DIAMONDS, PACKAGE_5_DIAMONDS);
 
   
   public static int getDiamondsForPackageName(String packageName) {
@@ -60,9 +57,6 @@ public class IAPValues {
     }
     if (packageName.equals(PACKAGE5)) {
       return PACKAGE_5_DIAMONDS;
-    }
-    if (packageName.equals(PACKAGE6)) {
-      return PACKAGE_6_DIAMONDS;
     }
     return 0;
   }
@@ -82,9 +76,6 @@ public class IAPValues {
     }
     if (packageName.equals(PACKAGE5)) {
       return PACKAGE_5_PRICE;
-    }
-    if (packageName.equals(PACKAGE6)) {
-      return PACKAGE_6_PRICE;
     }
     return 0;
   }
