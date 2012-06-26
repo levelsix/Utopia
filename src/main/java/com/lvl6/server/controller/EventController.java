9f11b9ff16e6167449bca223ce70bfcfa01f874f
bluestix <kelly@dekayd.com>
2012-06-26 14:11:59 -0700

    Fixed missing imports

--- src/main/java/com/lvl6/server/controller/EventController.java
+++ src/main/java/com/lvl6/server/controller/EventController.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.DependsOn;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.TransactionCallback;
 import org.springframework.transaction.support.TransactionTemplate;
 
 import com.lvl6.events.GameEvent;
