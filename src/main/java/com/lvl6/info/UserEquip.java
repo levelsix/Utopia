041d65ea50a648c9106f94f97a84801e315ab6bc
Kelly Roman <kelly@dekayd.com>
2012-07-10 14:34:02 -0700

    Making UserEquip serializable

--- src/main/java/com/lvl6/info/UserEquip.java
+++ src/main/java/com/lvl6/info/UserEquip.java
@@ -1,31 +1,36 @@
 package com.lvl6.info;
 
-public class UserEquip {
+import java.io.Serializable;
 
-  private int id;
-  private int userId;
-  private int equipId;
-  
-  public UserEquip(int id, int userId, int equipId) {
-    this.id = id;
-    this.userId = userId;
-    this.equipId = equipId;
-  }
+public class UserEquip implements Serializable {
 
-  public int getId() {
-    return id;
-  }
-  public int getUserId() {
-    return userId;
-  }
-  public int getEquipId() {
-    return equipId;
-  }
+	private static final long serialVersionUID = 7780574642033448043L;
+	private int id;
+	private int userId;
+	private int equipId;
 
-  @Override
-  public String toString() {
-    return "UserEquip [id=" + id + ", userId=" + userId
-        + ", equipId=" + equipId + "]";
-  }
-  
+	public UserEquip(int id, int userId, int equipId) {
+		this.id = id;
+		this.userId = userId;
+		this.equipId = equipId;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public int getEquipId() {
+		return equipId;
+	}
+
+	@Override
+	public String toString() {
+		return "UserEquip [id=" + id + ", userId=" + userId + ", equipId="
+				+ equipId + "]";
+	}
+
 }
