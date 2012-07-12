11f61142e5de7bb45105f9c92b2ce75475ce7c56
josefjohn <josefjohn88@gmail.com>
2012-07-11 23:32:50 -0700

    added controller constants for daily bonus chance of winning different equipment

--- src/main/java/com/lvl6/properties/ControllerConstants.java
+++ src/main/java/com/lvl6/properties/ControllerConstants.java
@@ -119,6 +119,11 @@ public class ControllerConstants {
   public static final int STARTUP__DAILY_BONUS_MIN_CONSEC_DAYS_SMALL_BONUS = 1;
   public static final int STARTUP__DAILY_BONUS_MIN_CONSEC_DAYS_BIG_BONUS = 5;
   public static final int STARTUP__DAILY_BONUS_MAX_CONSEC_DAYS_BIG_BONUS = 5;
+  public static final double STARTUP__DAILY_BONUS_PERCENTAGE_CHANCE_COMMON_EQUIP = 0;    //total should add up to 1
+  public static final double STARTUP__DAILY_BONUS_PERCENTAGE_CHANCE_UNCOMMON_EQUIP = 0.8;
+  public static final double STARTUP__DAILY_BONUS_PERCENTAGE_CHANCE_RARE_EQUIP = 0.15;
+  public static final double STARTUP__DAILY_BONUS_PERCENTAGE_CHANCE_EPIC_EQUIP = 0.05;
+  public static final double STARTUP__DAILY_BONUS_PERCENTAGE_CHANCE_LEGENDARY_EQUIP = 0;
   
   //ARMORY
   public static final double ARMORY__SELL_RATIO = 0.15;
