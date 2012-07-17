05f05bc8c494712d616db31bb015f30480eb7e9b
Kelly Roman <kelly@dekayd.com>
2012-07-16 22:04:23 -0700

    Added AdminUI test page

---
+++ src/test/java/com/lvl6/test/AdminUITest.java
@@ -0,0 +1,19 @@
+package com.lvl6.test;
+
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Test;
+
+import com.lvl6.ui.admin.pages.AdminPage;
+
+import junit.framework.TestCase;
+
+public class AdminUITest extends TestCase {
+	private WicketTester tester;
+	
+	
+	@Test
+	protected void testAdminPage() {
+		tester = new WicketTester();
+		tester.startPage(AdminPage.class);
+	}
+}
