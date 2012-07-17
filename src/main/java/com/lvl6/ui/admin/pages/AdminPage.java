0292f0db961a54b13fb552b409affc511233e101
Kelly Roman <kelly@dekayd.com>
2012-07-16 23:32:51 -0700

    Added stats to admin page

--- src/main/java/com/lvl6/ui/admin/pages/AdminPage.java
+++ src/main/java/com/lvl6/ui/admin/pages/AdminPage.java
@@ -1,7 +1,15 @@
 package com.lvl6.ui.admin.pages;
 
 
+import java.util.Map;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.model.Model;
+
+import com.hazelcast.core.HazelcastInstance;
+import com.lvl6.spring.AppContext;
 import com.lvl6.ui.admin.components.ReloadStaticDataLink;
+import com.lvl6.utils.ConnectedPlayer;
 
 public class AdminPage extends TemplatePage {
 
@@ -10,6 +18,14 @@ public class AdminPage extends TemplatePage {
 	public AdminPage() {
 		super();
 		add(new ReloadStaticDataLink("reloadStaticDataLink"));
+		setStats();
+	}
+
+	protected void setStats() {
+		Map<Integer, ConnectedPlayer> players = (Map<Integer, ConnectedPlayer>) AppContext.getApplicationContext().getBean("playersByPlayerId");
+		Integer connectedPlayers = players.size();
+		add(new Label("connectedPlayers", new Model<Integer>(connectedPlayers)));
+		add(new Label("totalPlayers", new Model<Integer>(0)));
 	}
 	
 	
