449001881d20e6ad567336850334e4864aab6e18
Kelly Roman <kelly@dekayd.com>
2012-07-16 22:14:03 -0700

    Added logging to admin UI

--- src/main/java/com/lvl6/ui/admin/components/ReloadStaticDataLink.java
+++ src/main/java/com/lvl6/ui/admin/components/ReloadStaticDataLink.java
@@ -1,19 +1,29 @@
 package com.lvl6.ui.admin.components;
 
+import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.link.Link;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.lvl6.utils.utilmethods.MiscMethods;
 
-public class ReloadStaticDataLink extends Link<String>{
+public class ReloadStaticDataLink extends Form<String>{
+	
+	Logger log = LoggerFactory.getLogger(getClass());
+	
+	
+	@Override
+	protected void onSubmit() {
+		super.onSubmit();
+		log.info("Reloading all static data");
+		MiscMethods.reloadAllRareChangeStaticData();
+	}
+
 	public ReloadStaticDataLink(String id) {
 		super(id);
 	}
 
 	private static final long serialVersionUID = -161974445690777238L;
 
-	@Override
-	public void onClick() {
-		MiscMethods.reloadAllRareChangeStaticData();
-	}
 
 }
