32061ac8ae588258d54284515cedc284276af7d5
Kelly Roman <kelly@dekayd.com>
2012-07-19 00:06:10 -0700

    Modified cassandra log4j appender... added unit tests

--- src/main/java/com/lvl6/cassandra/log4j/Log4jAppender.java
+++ src/main/java/com/lvl6/cassandra/log4j/Log4jAppender.java
@@ -2,12 +2,20 @@ package com.lvl6.cassandra.log4j;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 
+import me.prettyprint.cassandra.model.BasicColumnDefinition;
+import me.prettyprint.cassandra.model.BasicColumnFamilyDefinition;
+import me.prettyprint.cassandra.serializers.LongSerializer;
 import me.prettyprint.cassandra.serializers.StringSerializer;
 import me.prettyprint.cassandra.service.CassandraHostConfigurator;
+import me.prettyprint.cassandra.service.ThriftCfDef;
+import me.prettyprint.cassandra.service.ThriftCluster;
+import me.prettyprint.cassandra.service.ThriftColumnDef;
 import me.prettyprint.cassandra.service.ThriftKsDef;
 import me.prettyprint.cassandra.service.template.ColumnFamilyTemplate;
 import me.prettyprint.cassandra.service.template.ColumnFamilyUpdater;
@@ -15,12 +23,20 @@ import me.prettyprint.cassandra.service.template.ThriftColumnFamilyTemplate;
 import me.prettyprint.cassandra.utils.TimeUUIDUtils;
 import me.prettyprint.hector.api.Cluster;
 import me.prettyprint.hector.api.Keyspace;
+import me.prettyprint.hector.api.ddl.ColumnDefinition;
 import me.prettyprint.hector.api.ddl.ColumnFamilyDefinition;
+import me.prettyprint.hector.api.ddl.ColumnIndexType;
 import me.prettyprint.hector.api.ddl.ComparatorType;
 import me.prettyprint.hector.api.ddl.KeyspaceDefinition;
 import me.prettyprint.hector.api.exceptions.HectorException;
 import me.prettyprint.hector.api.factory.HFactory;
+import static me.prettyprint.hector.api.factory.HFactory.createColumn;
+import static me.prettyprint.hector.api.factory.HFactory.createKeyspace;
+import static me.prettyprint.hector.api.factory.HFactory.createMutator;
+import static me.prettyprint.hector.api.factory.HFactory.getOrCreateCluster;
 
+import org.apache.cassandra.thrift.Column;
+import org.apache.cassandra.thrift.ColumnDef;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.log4j.AppenderSkeleton;
 import org.apache.log4j.helpers.LogLog;
@@ -28,160 +44,226 @@ import org.apache.log4j.spi.LoggingEvent;
 
 public class Log4jAppender extends AppenderSkeleton {
 
-    private final static String CLUSTER_NAME = "log-cluster";
+	private static final StringSerializer se = new StringSerializer();
+	private static final LongSerializer le = new LongSerializer();
 
-    /**
-     * cassandra configuration
-     */
-    private String hosts;
-    private String instanceId;
-    private String keyspace;
-    private String columnFamily;
-    private int replicationFactor = 1;
+	private String clusterName;
 
-    /**
-     * active cluster connection
-     */
-    private Cluster cluster;
-    private ColumnFamilyTemplate<String, String> client;
+	private String hosts;
+	private String instanceId;
+	private String keyspace;
+	private String columnFamily;
+	private int replicationFactor = 1;
 
-    /**
-     * logger states
-     */
-    private boolean startedUp = false;
-    private boolean shutdown = false;
+	private ThriftCluster cluster;
+	private ColumnFamilyTemplate<String, String> client;
 
-    private long lastPublishTime = -1;
+	private boolean startedUp = false;
+	private boolean shutdown = false;
 
-    public Log4jAppender() {
-	super();
-	Runnable r = new Runnable() {
+	private long lastPublishTime = -1;
 
-	    public void run() {
-		while (!shutdown) {
-		    try {
-			Thread.sleep(1000);
-			if (client == null) {
-			    connect();
+	public Log4jAppender() {
+		super();
+		Runnable r = new Runnable() {
+
+			public void run() {
+				while (!shutdown) {
+					try {
+						Thread.sleep(1000);
+						if (client == null) {
+							connect();
+						}
+						if (lastPublishTime > 50) {
+							// TODO: some metrics to deactivate logging to this
+							// appender if its getting too time consuming, or
+							// queue
+						}
+					} catch (Exception e) {
+						LogLog.error("Log4jAppender", e);
+					} finally {
+						startedUp = true;
+					}
+				}
 			}
-			if (lastPublishTime > 50) {
-			    // TODO: some metrics to deactivate logging to this
-			    // appender if its getting too time consuming, or
-			    // queue
+		};
+		Thread alive = new Thread(r, "Cassandra-Log4J-Alive");
+		alive.setDaemon(true);
+		alive.start();
+	}
+
+	@Override
+	protected void append(LoggingEvent event) {
+		long startTime = System.currentTimeMillis();
+		String message = event.getMessage() + "";
+		if (startedUp && client != null) {
+			UUID key = TimeUUIDUtils.getUniqueTimeUUIDinMillis();
+			ColumnFamilyUpdater<String, String> updater = client.createUpdater(key.toString());
+			updater.setLong("time", event.getTimeStamp());
+			updater.setString("host", getHost());
+			updater.setString("message", message);
+			updater.setString("level", event.getLevel() + "");
+			updater.setString("name", event.getLoggerName());
+			updater.setString("thread", event.getThreadName());
+			if (event.getThrowableInformation() != null
+					&& event.getThrowableInformation().getThrowable() != null) {
+				String stacktrace = ExceptionUtils.getFullStackTrace(event
+						.getThrowableInformation().getThrowable());
+				updater.setString("stacktrace", stacktrace);
 			}
-		    } catch (Exception e) {
-			LogLog.error("Log4jAppender, " + e.getMessage());
-		    } finally {
-			startedUp = true;
-		    }
+			Map props = event.getProperties();
+			for (Object pkey : props.keySet()) {
+				updater.setString(pkey.toString(), props.get(pkey).toString());
+			}
+			String playerId = (String) event.getMDC("playerId");
+			if (playerId != null && !playerId.equals(""))
+				updater.setString("playerId", playerId);
+			String udId = (String) event.getMDC("udId");
+			if (playerId != null && !playerId.equals(""))
+				updater.setString("udid", udId.toString());
+			try {
+				client.update(updater);
+			} catch (HectorException e) {
+				client = null;
+				LogLog.error("append failed, " + e.getMessage());
+			}
+		} else {
+			if (startedUp) {
+				LogLog.warn("Log4jAppender, "
+						+ "cluster not available, skipping logging, " + message);
+			}
 		}
-	    }
-	};
-	Thread alive = new Thread(r, "Cassandra-Log4J-Alive");
-	alive.setDaemon(true);
-	alive.start();
-    }
-
-    @Override
-    protected void append(LoggingEvent event) {
-	long startTime = System.currentTimeMillis();
-	String message = event.getMessage() + "";
-	if (startedUp && client != null) {
-	    UUID key = TimeUUIDUtils.getUniqueTimeUUIDinMillis();
-	    ColumnFamilyUpdater<String, String> updater = client.createUpdater(key.toString());
-	    updater.setLong("time", event.getTimeStamp());
-	    updater.setString("host", getHost());
-	    updater.setString("message", message);
-	    updater.setString("level", event.getLevel() + "");
-	    updater.setString("name", event.getLoggerName());
-	    updater.setString("thread", event.getThreadName());
-	    if(event.getThrowableInformation() != null && event.getThrowableInformation().getThrowable() != null) {
-	    	String stacktrace = ExceptionUtils.getFullStackTrace(event.getThrowableInformation().getThrowable());
-	    	updater.setString("stacktrace", stacktrace);
-	    }
-	    Map props = event.getProperties();
-	    for(Object pkey : props.keySet()) {
-	    	updater.setString(pkey.toString(), props.get(pkey).toString());
-	    }
-	    String playerId = (String) event.getMDC("playerId");
-	    if(playerId != null && !playerId.equals(""))
-	    	updater.setString("playerId", playerId);
-	    String udId = (String) event.getMDC("udId");
-	    if(playerId != null && !playerId.equals(""))
-	    	updater.setString("udid", udId.toString());
-	    try {
-		client.update(updater);
-	    } catch (HectorException e) {
-		client = null;
-		LogLog.error("append failed, " + e.getMessage());
-	    }
-	} else {
-	    if (startedUp) {
-		LogLog.warn("Log4jAppender, " + "cluster not available, skipping logging, " + message);
-	    }
+		long endTime = System.currentTimeMillis();
+		lastPublishTime = endTime - startTime;
 	}
-	long endTime = System.currentTimeMillis();
-	lastPublishTime = endTime - startTime;
-    }
 
-    public void close() {
-	shutdown = true;
-	cluster.getConnectionManager().shutdown();
-    }
-
-    private void connect() throws HectorException {
-	LogLog.debug("creating cassandra cluster connection");
-	CassandraHostConfigurator cassandraHostConfigurator = new CassandraHostConfigurator(hosts);
-	cassandraHostConfigurator.setMaxActive(20);
-	cassandraHostConfigurator.setCassandraThriftSocketTimeout(500);
-	cassandraHostConfigurator.setMaxWaitTimeWhenExhausted(500);
-	Cluster c = HFactory.getOrCreateCluster(CLUSTER_NAME, cassandraHostConfigurator);
-	KeyspaceDefinition keyspaceDef = c.describeKeyspace(keyspace);
-	if (keyspaceDef == null) {
-	    ColumnFamilyDefinition cfDef = HFactory.createColumnFamilyDefinition(keyspace, columnFamily, ComparatorType.BYTESTYPE);
-	    KeyspaceDefinition newKeyspace = HFactory.createKeyspaceDefinition(keyspace, ThriftKsDef.DEF_STRATEGY_CLASS, replicationFactor,
-		    Arrays.asList(cfDef));
-	    c.addKeyspace(newKeyspace, false);
+	public void close() {
+		LogLog.warn("Closing log4cassandra appender");
+		shutdown = true;
+		cluster.getConnectionManager().shutdown();
 	}
-	Keyspace ksp = HFactory.createKeyspace(keyspace, c);
-	cluster = c;
-	client = new ThriftColumnFamilyTemplate<String, String>(ksp, columnFamily, StringSerializer.get(), StringSerializer.get());
-    }
 
-    private String getHost() {
-	if (instanceId != null) {
-	    return instanceId;
+	private void connect() throws Exception {
+		LogLog.debug("creating cassandra cluster connection");
+		CassandraHostConfigurator cassandraHostConfigurator = new CassandraHostConfigurator(hosts);
+		cassandraHostConfigurator.setMaxActive(20);
+		cassandraHostConfigurator.setCassandraThriftSocketTimeout(500);
+		cassandraHostConfigurator.setMaxWaitTimeWhenExhausted(500);
+		ThriftCluster c = new ThriftCluster(clusterName, cassandraHostConfigurator);//getOrCreateCluster(getClusterName(), cassandraHostConfigurator);
+		setupColumnFamilies(c);
+		Keyspace ksp = createKeyspace(keyspace, c);
+		cluster = c;
+		client = new ThriftColumnFamilyTemplate<String, String>(ksp,
+				columnFamily, StringSerializer.get(), StringSerializer.get());
 	}
-	try {
-	    String host = InetAddress.getLocalHost().getHostAddress();
-	    return host;
-	} catch (UnknownHostException e) {
+
+	private void setupColumnFamilies(Cluster c) throws Exception {
+		KeyspaceDefinition keyspaceDef = c.describeKeyspace(keyspace);
+		BasicColumnFamilyDefinition columnFamilyDefinition = new BasicColumnFamilyDefinition();
+		columnFamilyDefinition.setKeyspaceName(keyspace);
+		columnFamilyDefinition.setName(columnFamily);
+		ColumnFamilyDefinition cfDef = new ThriftCfDef(columnFamilyDefinition);
+		if (keyspaceDef == null) {
+			LogLog.warn("Creating keyspace "+keyspace);
+			KeyspaceDefinition newKeyspace = HFactory.createKeyspaceDefinition(
+					keyspace,
+					ThriftKsDef.DEF_STRATEGY_CLASS,
+					replicationFactor,
+					Arrays.asList(cfDef)
+			);
+			c.addKeyspace(newKeyspace);
+		}else {
+			LogLog.warn(keyspaceDef.getName()+" exists");
+		}
+		keyspaceDef = null;
+		cfDef = null;
+		KeyspaceDefinition fromCluster = c.describeKeyspace(keyspace);
+		for(ColumnFamilyDefinition cfd : fromCluster.getCfDefs()) {
+			if(cfd.getName().equals(columnFamily)) {
+				cfDef = cfd;
+			}
+		}
+		columnFamilyDefinition = new BasicColumnFamilyDefinition(cfDef);
+		//level index
+        BasicColumnDefinition bcdf = new BasicColumnDefinition();
+        bcdf.setName(StringSerializer.get().toByteBuffer("level"));
+        bcdf.setIndexName("level_index");
+        bcdf.setIndexType(ColumnIndexType.KEYS);
+        bcdf.setValidationClass(ComparatorType.UTF8TYPE.getClassName());
+        columnFamilyDefinition.addColumnDefinition(bcdf);
+        //time index
+        BasicColumnDefinition bcdf2 = new BasicColumnDefinition();
+        bcdf2.setName(StringSerializer.get().toByteBuffer("time"));
+        bcdf2.setIndexName("time_index");
+        bcdf2.setIndexType(ColumnIndexType.KEYS);
+        bcdf2.setValidationClass(ComparatorType.LONGTYPE.getClassName());
+        columnFamilyDefinition.addColumnDefinition(bcdf2);
+        cfDef = new ThriftCfDef(columnFamilyDefinition);
+        c.updateColumnFamily(cfDef);
+		keyspaceDef = c.describeKeyspace(keyspace);
+		for(ColumnFamilyDefinition cfd : keyspaceDef.getCfDefs()) {
+			LogLog.warn("ColumnFamilyDefinition: "+cfd.getName()+":"+cfd.getColumnType().getValue());
+			for(ColumnDefinition cd : cfd.getColumnMetadata()) {
+				LogLog.warn("Index name: "+cd.getIndexName()+", IndexType: "+cd.getIndexType());
+			}
+		}
+//		assertEquals("level",StringSerializer.get().fromByteBuffer(keyspaceDef.getCfDefs().get(0).getColumnMetadata().get(0).getName()));
+//	    assertEquals("level_index",keyspaceDef.getCfDefs().get(0).getColumnMetadata().get(0).getIndexName());
+//	    assertEquals("time",StringSerializer.get().fromByteBuffer(keyspaceDef.getCfDefs().get(0).getColumnMetadata().get(0).getName()));
+//	    assertEquals("time_index",keyspaceDef.getCfDefs().get(0).getColumnMetadata().get(0).getIndexName());
 	}
-	return "";
-    }
+	
+	
+	
 
-    public boolean requiresLayout() {
-	return false;
-    }
+	private void assertEquals(String string, String fromByteBuffer) throws Exception {
+		if(!string.equals(fromByteBuffer)) {
+			throw new Exception("Error creating index on: "+string);
+		}
+	}
 
-    public void setColumnFamily(String columnFamily) {
-	this.columnFamily = columnFamily;
-    }
+	private String getHost() {
+		if (instanceId != null) {
+			return instanceId;
+		}
+		try {
+			String host = InetAddress.getLocalHost().getHostAddress();
+			return host;
+		} catch (UnknownHostException e) {
+		}
+		return "";
+	}
 
-    public void setHosts(String hosts) {
-	this.hosts = hosts;
-    }
+	public boolean requiresLayout() {
+		return false;
+	}
 
-    public void setInstanceId(String instanceId) {
-	this.instanceId = instanceId;
-    }
+	public void setColumnFamily(String columnFamily) {
+		this.columnFamily = columnFamily;
+	}
 
-    public void setKeyspace(String keyspace) {
-	this.keyspace = keyspace;
-    }
+	public void setHosts(String hosts) {
+		this.hosts = hosts;
+	}
 
-    public void setReplicationFactor(int replicationFactor) {
-	this.replicationFactor = replicationFactor;
-    }
+	public void setInstanceId(String instanceId) {
+		this.instanceId = instanceId;
+	}
+
+	public void setKeyspace(String keyspace) {
+		this.keyspace = keyspace;
+	}
+
+	public void setReplicationFactor(int replicationFactor) {
+		this.replicationFactor = replicationFactor;
+	}
+
+	public String getClusterName() {
+		return clusterName;
+	}
+
+	public void setClusterName(String clusterName) {
+		this.clusterName = clusterName;
+	}
 
 }
---
+++ src/test/java/com/lvl6/test/CassandraTest.java
@@ -0,0 +1,89 @@
+package com.lvl6.test;
+
+import java.net.UnknownHostException;
+import java.util.Arrays;
+
+import me.prettyprint.cassandra.model.BasicColumnDefinition;
+import me.prettyprint.cassandra.model.BasicColumnFamilyDefinition;
+import me.prettyprint.cassandra.serializers.StringSerializer;
+import me.prettyprint.cassandra.service.CassandraHostConfigurator;
+import me.prettyprint.cassandra.service.ThriftCfDef;
+import me.prettyprint.cassandra.service.ThriftCluster;
+import me.prettyprint.hector.api.ddl.ColumnFamilyDefinition;
+import me.prettyprint.hector.api.ddl.ColumnIndexType;
+import me.prettyprint.hector.api.ddl.ComparatorType;
+import me.prettyprint.hector.api.ddl.KeyspaceDefinition;
+import me.prettyprint.hector.api.factory.HFactory;
+
+import org.apache.cassandra.thrift.NotFoundException;
+import org.apache.thrift.TException;
+import org.apache.thrift.transport.TTransportException;
+import org.apache.wicket.util.tester.WicketTester;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.lvl6.ui.admin.pages.AdminPage;
+import com.lvl6.ui.admin.pages.MainPage;
+
+import junit.framework.TestCase;
+
+public class CassandraTest extends TestCase {
+
+	Logger log = LoggerFactory.getLogger(getClass());
+
+	private ThriftCluster cassandraCluster;
+	private CassandraHostConfigurator cassandraHostConfigurator;
+
+	//@Before
+	public void setupCase() throws TTransportException, TException,
+			IllegalArgumentException, NotFoundException, UnknownHostException,
+			Exception {
+		cassandraHostConfigurator = new CassandraHostConfigurator(
+				"lucidyang.dekayd.com");
+		cassandraCluster = new ThriftCluster("Test Cluster",
+				cassandraHostConfigurator);
+	}
+	
+	@Test
+	  public void testEditColumnFamily() throws Exception {
+		setupCase();
+	    try {
+			BasicColumnFamilyDefinition columnFamilyDefinition = new BasicColumnFamilyDefinition();
+		    columnFamilyDefinition.setKeyspaceName("DynKeyspace3");
+		    columnFamilyDefinition.setName("DynamicCF");
+		    
+		    ColumnFamilyDefinition cfDef = new ThriftCfDef(columnFamilyDefinition);
+		    
+		    KeyspaceDefinition keyspaceDefinition =
+		        HFactory.createKeyspaceDefinition("DynKeyspace3", "org.apache.cassandra.locator.SimpleStrategy", 1, Arrays.asList(cfDef));
+		    cassandraCluster.addKeyspace(keyspaceDefinition);
+	    }catch(Exception e) {
+	    	
+	    }
+	    KeyspaceDefinition fromCluster = cassandraCluster.describeKeyspace("DynKeyspace3");
+	    ColumnFamilyDefinition cfDef = fromCluster.getCfDefs().get(0);
+	    
+	    BasicColumnFamilyDefinition columnFamilyDefinition = new BasicColumnFamilyDefinition(cfDef);
+	    BasicColumnDefinition columnDefinition = new BasicColumnDefinition();
+	    columnDefinition.setName(StringSerializer.get().toByteBuffer("birthdate"));
+	    columnDefinition.setIndexName("birthdate_idx");
+	    columnDefinition.setIndexType(ColumnIndexType.KEYS);
+	    columnDefinition.setValidationClass(ComparatorType.LONGTYPE.getClassName());
+	    columnFamilyDefinition.addColumnDefinition(columnDefinition);
+	    
+	    columnDefinition = new BasicColumnDefinition();
+	    columnDefinition.setName(StringSerializer.get().toByteBuffer("nonindexed_field"));
+	    columnDefinition.setValidationClass(ComparatorType.LONGTYPE.getClassName());
+	    columnFamilyDefinition.addColumnDefinition(columnDefinition);
+	    
+	    cassandraCluster.updateColumnFamily(new ThriftCfDef(columnFamilyDefinition));
+	    
+	    fromCluster = cassandraCluster.describeKeyspace("DynKeyspace3");
+	    
+	    assertEquals("birthdate",StringSerializer.get().fromByteBuffer(fromCluster.getCfDefs().get(0).getColumnMetadata().get(0).getName()));
+	    assertEquals("birthdate_idx",fromCluster.getCfDefs().get(0).getColumnMetadata().get(0).getIndexName());
+	    assertEquals("nonindexed_field",StringSerializer.get().fromByteBuffer(fromCluster.getCfDefs().get(0).getColumnMetadata().get(1).getName()));
+	  }
+}
