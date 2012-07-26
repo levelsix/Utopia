fa4940af19c10075d53edaf244687c57e104384b
Kelly Roman <kelly@dekayd.com>
2012-07-25 17:51:03 -0700

    Adding top spenders and top purchases to StatsUtil

---
+++ src/main/java/com/lvl6/stats/InAppPurchase.java
@@ -0,0 +1,29 @@
+package com.lvl6.stats;
+
+import java.util.Date;
+
+public class InAppPurchase {
+	protected Integer userId;
+	protected Double cashSpent;
+	protected Date purchasedDate;
+
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public Double getCashSpent() {
+		return cashSpent;
+	}
+	public void setCashSpent(Double cashSpent) {
+		this.cashSpent = cashSpent;
+	}
+	public Date getPurchasedDate() {
+		return purchasedDate;
+	}
+	public void setPurchasedDate(Date purchasedDate) {
+		this.purchasedDate = purchasedDate;
+	}
+}
