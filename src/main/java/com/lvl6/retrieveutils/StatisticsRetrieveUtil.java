5cd021b793a9b8625e94f850f53ad16ad9cec777
Conrad Chan <conradchan@gmail.com>
2012-07-26 17:23:58 -0700

    changed statistics to use DBConstants values

--- src/main/java/com/lvl6/retrieveutils/StatisticsRetrieveUtil.java
+++ src/main/java/com/lvl6/retrieveutils/StatisticsRetrieveUtil.java
@@ -2,8 +2,6 @@ package com.lvl6.retrieveutils;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.Collection;
-import java.util.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -36,45 +34,51 @@ public class StatisticsRetrieveUtil {
 	
 	
 	public Integer countPayingPlayers() {
-		return jdbcTemplate.queryForInt("select count(*) from users where id in (select user_id from iap_history where premiumcur_purchased > 0)");
+		return jdbcTemplate.queryForInt("select count(*) from " + DBConstants.TABLE_USER + " where " + DBConstants.USER__ID +
+		    " in (select " + DBConstants.IAP_HISTORY__USER_ID + " from " + DBConstants.TABLE_IAP_HISTORY + " where " + 
+		    DBConstants.IAP_HISTORY__PREMIUMCUR_PURCHASED + " > 0)");
 	}
 	
 	public Long countInAppPurchases() {
-		return jdbcTemplate.queryForLong("select count(*) from iap_history where premiumcur_purchased > 0");
+		return jdbcTemplate.queryForLong("select count(*) from " + DBConstants.TABLE_IAP_HISTORY + " where " + DBConstants.IAP_HISTORY__PREMIUMCUR_PURCHASED + " > 0");
 	}
 	
 	public Long sumOfInAppPurchases() {
-		return jdbcTemplate.queryForLong("select sum(cash_spent) from iap_history");
+		return jdbcTemplate.queryForLong("select sum(" + DBConstants.IAP_HISTORY__CASH_SPENT + ") from " + DBConstants.TABLE_IAP_HISTORY);
 	}
 	
 	public Long countNumberKiipRewardsRedeemed() {
-		return jdbcTemplate.queryForLong("select count(*) from kiip_reward_history");
+		return jdbcTemplate.queryForLong("select count(*) from " + DBConstants.TABLE_KIIP_REWARD_HISTORY);
 	}
 	
 	public Long countMarketplaceTransactions() {
-		return jdbcTemplate.queryForLong("select count(*) from marketplace_transaction_history");
+		return jdbcTemplate.queryForLong("select count(*) from " + DBConstants.TABLE_MARKETPLACE_TRANSACTION_HISTORY);
 	}
 	
 	public Long countMarketplacePosts() {
-		return jdbcTemplate.queryForLong("select count(*) from marketplace");
+		return jdbcTemplate.queryForLong("select count(*) from " + DBConstants.TABLE_MARKETPLACE);
 	}
 	
 	public Long sumOfSilverInWorld() {
-		return jdbcTemplate.queryForLong("select sum(coins) + sum(vault_balance) from users where is_fake=0");
+		return jdbcTemplate.queryForLong("select sum(" + DBConstants.USER__COINS +") + sum(" + DBConstants.USER__VAULT_BALANCE + ") from " + DBConstants.TABLE_USER 
+		    + " where " + DBConstants.USER__IS_FAKE + "=0");
 	}
 	
 	public Long sumOfDiamondsInWorld() {
-		return jdbcTemplate.queryForLong("select sum(diamonds) from users where is_fake=0");
+		return jdbcTemplate.queryForLong("select sum(" + DBConstants.USER__DIAMONDS + ") from " + DBConstants.TABLE_USER + " where " + DBConstants.USER__IS_FAKE + "=0");
 	}
 	
 	public List<Spender> getTopSpenders(Integer limit){
+	  final String amountSpent = "amount_spent";
 		List<Spender> spenders = this.jdbcTemplate.query(
-		        "select user_id, sum(cash_spent) as amount_spent from iap_history group by user_id order by sum(cash_spent) desc limit "+limit,
+		        "select " + DBConstants.IAP_HISTORY__USER_ID + ", sum(" + DBConstants.IAP_HISTORY__CASH_SPENT + ") as " + amountSpent + "  from " +
+		            DBConstants.TABLE_IAP_HISTORY + " group by " + DBConstants.IAP_HISTORY__USER_ID + " order by sum(" + DBConstants.IAP_HISTORY__CASH_SPENT + 
+		            ") desc limit "+limit,
 		        new RowMapper<Spender>() {
 		            public Spender mapRow(ResultSet rs, int rowNum) throws SQLException {
 		            	Spender spender = new Spender();
-		            	spender.setUserId(rs.getInt("user_id"));
-		            	spender.setAmountSpent(rs.getDouble("amount_spent"));
+		            	spender.setUserId(rs.getInt(DBConstants.IAP_HISTORY__USER_ID));
+		            	spender.setAmountSpent(rs.getDouble(amountSpent));
 		                return spender;
 		            }
 		        });
@@ -83,13 +87,15 @@ public class StatisticsRetrieveUtil {
 	
 	public List<InAppPurchase> getTopInAppPurchases(Integer limit){
 		List<InAppPurchase> inAppPurchases = this.jdbcTemplate.query(
-		        "select user_id, cash_spent, purchase_date from iap_history order by purchase_date desc limit "+limit,
+		        "select " + DBConstants.IAP_HISTORY__USER_ID + ", " + DBConstants.IAP_HISTORY__CASH_SPENT + ", " + DBConstants.IAP_HISTORY__PURCHASE_DATE + 
+		        " from " + DBConstants.TABLE_IAP_HISTORY + 
+		        " order by " + DBConstants.IAP_HISTORY__PURCHASE_DATE + " desc limit "+limit,
 		        new RowMapper<InAppPurchase>() {
 		            public InAppPurchase mapRow(ResultSet rs, int rowNum) throws SQLException {
 		            	InAppPurchase inAppPurchase = new InAppPurchase();
-		            	inAppPurchase.setUserId(rs.getInt("user_id"));
-		            	inAppPurchase.setCashSpent(rs.getDouble("cash_spent"));
-		            	inAppPurchase.setPurchasedDate(rs.getDate("purchase_date"));
+		            	inAppPurchase.setUserId(rs.getInt(DBConstants.IAP_HISTORY__USER_ID));
+		            	inAppPurchase.setCashSpent(rs.getDouble(DBConstants.IAP_HISTORY__CASH_SPENT));
+		            	inAppPurchase.setPurchasedDate(rs.getDate(DBConstants.IAP_HISTORY__PURCHASE_DATE));
 		                return inAppPurchase;
 		            }
 		        });
