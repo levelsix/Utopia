8522300fa11ec6fe0c59c9b82ae96513351a7890
Conrad Chan <conradchan@gmail.com>
2012-07-27 17:57:56 -0700

    fixed application stat bug

--- src/main/java/com/lvl6/ui/admin/components/ApplicationStats.java
+++ src/main/java/com/lvl6/ui/admin/components/ApplicationStats.java
@@ -1,93 +1,98 @@
 package com.lvl6.ui.admin.components;
 
 public class ApplicationStats {
-	protected Integer connectedPlayersCount = 0;
-	protected Integer totalPlayersCount = 0;
-	protected Integer totalPayingPlayers = 0;
-	protected Long totalInAppPurchases = 0l;
-	protected Long sumOfInAppPurchases = 0l;
-	protected Long countNumberKiipRewardsRedeemed = 0l;
-	protected Long countMarketplaceTransactions = 0l;
-	protected Long countMarketplacePosts = 0l;
-	protected Long sumOfSilverInWorld = 0l;
-	protected Long sumOfDiamondsInWorld = 0l;
-	
-	
-	public Integer getPercentageOfPlayersPaying() {
-		return 100*(getTotalPayingPlayers()/getTotalPlayersCount());
-	}
-	public void setPercentageOfPlayersPaying(Integer value) {
-	}
-	
-	
-	public Long getAverageSilverPerPlayer() {
-		return getSumOfSilverInWorld()/getTotalPlayersCount();
-	}
-	public void setAverageSilverPerPlayer(Long value) {
-	}
-	
-	public Integer getTotalPayingPlayers() {
-		return totalPayingPlayers;
-	}
-	public void setTotalPayingPlayers(Integer totalPayingPlayers) {
-		this.totalPayingPlayers = totalPayingPlayers;
-	}
-	public Long getTotalInAppPurchases() {
-		return totalInAppPurchases;
-	}
-	public void setTotalInAppPurchases(Long totalInAppPurchases) {
-		this.totalInAppPurchases = totalInAppPurchases;
-	}
-	public Long getSumOfInAppPurchases() {
-		return sumOfInAppPurchases;
-	}
-	public void setSumOfInAppPurchases(Long sumOfInAppPurchases) {
-		this.sumOfInAppPurchases = sumOfInAppPurchases;
-	}
-	public Long getCountNumberKiipRewardsRedeemed() {
-		return countNumberKiipRewardsRedeemed;
-	}
-	public void setCountNumberKiipRewardsRedeemed(
-			Long countNumberKiipRewardsRedeemed) {
-		this.countNumberKiipRewardsRedeemed = countNumberKiipRewardsRedeemed;
-	}
-	public Long getCountMarketplaceTransactions() {
-		return countMarketplaceTransactions;
-	}
-	public void setCountMarketplaceTransactions(Long countMarketplaceTransactions) {
-		this.countMarketplaceTransactions = countMarketplaceTransactions;
-	}
-	public Long getCountMarketplacePosts() {
-		return countMarketplacePosts;
-	}
-	public void setCountMarketplacePosts(Long countMarketplacePosts) {
-		this.countMarketplacePosts = countMarketplacePosts;
-	}
-	public Long getSumOfSilverInWorld() {
-		return sumOfSilverInWorld;
-	}
-	public void setSumOfSilverInWorld(Long sumOfSilverInWorld) {
-		this.sumOfSilverInWorld = sumOfSilverInWorld;
-	}
-	public Long getSumOfDiamondsInWorld() {
-		return sumOfDiamondsInWorld;
-	}
-	public void setSumOfDiamondsInWorld(Long sumOfDiamondsInWorld) {
-		this.sumOfDiamondsInWorld = sumOfDiamondsInWorld;
-	}
+  protected Integer connectedPlayersCount = 0;
+  protected Integer totalPlayersCount = 0;
+  protected Integer totalPayingPlayers = 0;
+  protected Long totalInAppPurchases = 0l;
+  protected Long sumOfInAppPurchases = 0l;
+  protected Long countNumberKiipRewardsRedeemed = 0l;
+  protected Long countMarketplaceTransactions = 0l;
+  protected Long countMarketplacePosts = 0l;
+  protected Long sumOfSilverInWorld = 0l;
+  protected Long sumOfDiamondsInWorld = 0l;
 
-	public Integer getConnectedPlayersCount() {
-		return connectedPlayersCount;
-	}
-	public void setConnectedPlayersCount(Integer connectedPlayersCount) {
-		this.connectedPlayersCount = connectedPlayersCount;
-	}
-	
-	public Integer getTotalPlayersCount() {
-		return totalPlayersCount;
-	}
-	public void setTotalPlayersCount(Integer totalPlayersCount) {
-		this.totalPlayersCount = totalPlayersCount;
-	}
-	
+
+  public Integer getPercentageOfPlayersPaying() {
+    if (getTotalPlayersCount() == 0) {
+      return 0;
+    }
+    return 100*(getTotalPayingPlayers()/getTotalPlayersCount());
+  }
+  public void setPercentageOfPlayersPaying(Integer value) {
+  }
+
+  public Long getAverageSilverPerPlayer() {
+    if (getTotalPlayersCount() == 0) {
+      return 0L;
+    }
+    return getSumOfSilverInWorld()/getTotalPlayersCount();
+  }
+  public void setAverageSilverPerPlayer(Long value) {
+  }
+
+  public Integer getTotalPayingPlayers() {
+    return totalPayingPlayers;
+  }
+  public void setTotalPayingPlayers(Integer totalPayingPlayers) {
+    this.totalPayingPlayers = totalPayingPlayers;
+  }
+  public Long getTotalInAppPurchases() {
+    return totalInAppPurchases;
+  }
+  public void setTotalInAppPurchases(Long totalInAppPurchases) {
+    this.totalInAppPurchases = totalInAppPurchases;
+  }
+  public Long getSumOfInAppPurchases() {
+    return sumOfInAppPurchases;
+  }
+  public void setSumOfInAppPurchases(Long sumOfInAppPurchases) {
+    this.sumOfInAppPurchases = sumOfInAppPurchases;
+  }
+  public Long getCountNumberKiipRewardsRedeemed() {
+    return countNumberKiipRewardsRedeemed;
+  }
+  public void setCountNumberKiipRewardsRedeemed(
+      Long countNumberKiipRewardsRedeemed) {
+    this.countNumberKiipRewardsRedeemed = countNumberKiipRewardsRedeemed;
+  }
+  public Long getCountMarketplaceTransactions() {
+    return countMarketplaceTransactions;
+  }
+  public void setCountMarketplaceTransactions(Long countMarketplaceTransactions) {
+    this.countMarketplaceTransactions = countMarketplaceTransactions;
+  }
+  public Long getCountMarketplacePosts() {
+    return countMarketplacePosts;
+  }
+  public void setCountMarketplacePosts(Long countMarketplacePosts) {
+    this.countMarketplacePosts = countMarketplacePosts;
+  }
+  public Long getSumOfSilverInWorld() {
+    return sumOfSilverInWorld;
+  }
+  public void setSumOfSilverInWorld(Long sumOfSilverInWorld) {
+    this.sumOfSilverInWorld = sumOfSilverInWorld;
+  }
+  public Long getSumOfDiamondsInWorld() {
+    return sumOfDiamondsInWorld;
+  }
+  public void setSumOfDiamondsInWorld(Long sumOfDiamondsInWorld) {
+    this.sumOfDiamondsInWorld = sumOfDiamondsInWorld;
+  }
+
+  public Integer getConnectedPlayersCount() {
+    return connectedPlayersCount;
+  }
+  public void setConnectedPlayersCount(Integer connectedPlayersCount) {
+    this.connectedPlayersCount = connectedPlayersCount;
+  }
+
+  public Integer getTotalPlayersCount() {
+    return totalPlayersCount;
+  }
+  public void setTotalPlayersCount(Integer totalPlayersCount) {
+    this.totalPlayersCount = totalPlayersCount;
+  }
+
 }
