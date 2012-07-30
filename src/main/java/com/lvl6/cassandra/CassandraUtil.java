93bd4f5e7da69c5d2d6164a2ff9af4e1e7cb8023
Kelly Roman <kelly@dekayd.com>
2012-07-30 13:58:19 -0700

    Fixing NPE on GameEventHandler.updatePlayerToServerMap

--- src/main/java/com/lvl6/cassandra/CassandraUtil.java
+++ src/main/java/com/lvl6/cassandra/CassandraUtil.java
@@ -1,11 +1,21 @@
 package com.lvl6.cassandra;
 
 import me.prettyprint.cassandra.model.BasicColumnDefinition;
+import me.prettyprint.hector.api.Keyspace;
 import me.prettyprint.hector.api.ddl.ComparatorType;
 
 public interface CassandraUtil {
 
 	public abstract BasicColumnDefinition createBasicColumnDefinition(String name,
 			ComparatorType type, boolean index);
+	
+	public abstract void insertCounterColumn(Keyspace keyspace, String key, String columnFamily, String column, Long initialValue);
+	public abstract void insertCounterColumn(Keyspace keyspace, String key, String columnFamily, Long column, Long initialValue);
+	
+	public abstract void incrementCounter(Keyspace keyspace, String key, String columnFamily, String column, Long increment);
+	public abstract void incrementCounter(Keyspace keyspace, String key, String columnFamily, Long column, Long increment);
+	
+	public abstract Long getCounterValue(Keyspace keyspace, String key, String columnFamily, String column, Long increment);
+	public abstract Long getCounterValue(Keyspace keyspace, String key, String columnFamily, Long column, Long increment);
 
 }
--- src/main/java/com/lvl6/cassandra/CassandraUtilImpl.java
+++ src/main/java/com/lvl6/cassandra/CassandraUtilImpl.java
@@ -1,14 +1,30 @@
 package com.lvl6.cassandra;
 
 import me.prettyprint.cassandra.model.BasicColumnDefinition;
+import me.prettyprint.cassandra.model.thrift.ThriftCounterColumnQuery;
+import static me.prettyprint.hector.api.factory.HFactory.createColumn;
+import static me.prettyprint.hector.api.factory.HFactory.createCounterColumn;
+import static me.prettyprint.hector.api.factory.HFactory.createColumnQuery;
+import static me.prettyprint.hector.api.factory.HFactory.createKeyspace;
+import static me.prettyprint.hector.api.factory.HFactory.createMutator;
+import static me.prettyprint.hector.api.factory.HFactory.createSuperColumn;
+import static me.prettyprint.hector.api.factory.HFactory.getOrCreateCluster;
+import me.prettyprint.cassandra.serializers.LongSerializer;
 import me.prettyprint.cassandra.serializers.StringSerializer;
+import me.prettyprint.hector.api.Keyspace;
 import me.prettyprint.hector.api.ddl.ColumnIndexType;
 import me.prettyprint.hector.api.ddl.ComparatorType;
+import me.prettyprint.hector.api.mutation.MutationResult;
+import me.prettyprint.hector.api.mutation.Mutator;
+import me.prettyprint.hector.api.query.CounterQuery;
 
 public class CassandraUtilImpl implements CassandraUtil {
 	/* (non-Javadoc)
 	 * @see com.lvl6.cassandra.CassandraUtil#createBasicColumn(java.lang.String, me.prettyprint.hector.api.ddl.ComparatorType, boolean)
 	 */
+	protected StringSerializer ss = StringSerializer.get();
+	protected LongSerializer ls = LongSerializer.get();
+	
 	@Override
 	public BasicColumnDefinition createBasicColumnDefinition(String name, ComparatorType type, boolean index){
 		BasicColumnDefinition bcdf = new BasicColumnDefinition();
@@ -20,4 +36,56 @@ public class CassandraUtilImpl implements CassandraUtil {
 		bcdf.setValidationClass(type.getClassName());
 		return bcdf;
 	}
+
+	@Override
+	public void insertCounterColumn(Keyspace keyspace, String key,
+			String columnFamily, String column, Long initialValue) {
+		Mutator<String> m = createMutator(keyspace, ss);
+		MutationResult mr = m.insertCounter(key, columnFamily, createCounterColumn(column, initialValue));
+	}
+
+	@Override
+	public void insertCounterColumn(Keyspace keyspace, String key,
+			String columnFamily, Long column, Long initialValue) {
+		Mutator<String> m = createMutator(keyspace, ss);
+		MutationResult mr = m.insertCounter(key, columnFamily, createCounterColumn(column, initialValue, LongSerializer.get()));		
+	}
+
+	@Override
+	public void incrementCounter(Keyspace keyspace, String key,
+			String columnFamily, String column, Long increment) {
+		Mutator<String> m = createMutator(keyspace, ss);
+		m.incrementCounter(key, columnFamily, column, increment);
+	}
+
+	@Override
+	public void incrementCounter(Keyspace keyspace, String key,
+			String columnFamily, Long column, Long increment) {
+		Mutator<String> m = createMutator(keyspace, ss);
+		m.incrementCounter(key, columnFamily, column, increment);
+		
+	}
+
+	@Override
+	public Long getCounterValue(Keyspace keyspace, String key,
+			String columnFamily, String column, Long increment) {
+		CounterQuery<String, String> counter = new ThriftCounterColumnQuery<String,String>(keyspace, ss, ss);
+		counter.setColumnFamily(columnFamily).setKey(key).setName(column);
+		return counter.execute().get().getValue();
+	}
+
+	@Override
+	public Long getCounterValue(Keyspace keyspace, String key,
+			String columnFamily, Long column, Long increment) {
+		CounterQuery<String, Long> counter = new ThriftCounterColumnQuery<String,Long>(keyspace, ss, ls);
+		counter.setColumnFamily(columnFamily).setKey(key).setName(column);
+		return counter.execute().get().getValue();
+	}
+	
+	
+	
+/*	
+*/
+	
+	
 }
