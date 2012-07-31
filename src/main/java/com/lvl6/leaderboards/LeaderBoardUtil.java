b1976f6789169a459e24d1c564d25fcaa2f418df
Kelly Roman <kelly@dekayd.com>
2012-07-30 18:14:12 -0700

    Adding leaderboard classes

---
+++ src/main/java/com/lvl6/leaderboards/LeaderBoardUtil.java
@@ -0,0 +1,12 @@
+package com.lvl6.leaderboards;
+
+import redis.clients.jedis.Jedis;
+
+public class LeaderBoardUtil {
+	
+	protected Lvl6Jedis jedis;
+	
+	protected void testJedis() {
+		jedis.
+	}
+}
