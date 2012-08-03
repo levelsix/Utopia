a52c228b8052dc3b7d5cc4ff4db3eb481d017dad
Kelly Roman <kelly@dekayd.com>
2012-08-03 01:47:47 -0700

    Adding load testing classes

--- src/main/java/com/lvl6/loadtesting/GameEventRecorder.java
+++ src/main/java/com/lvl6/loadtesting/GameEventRecorder.java
@@ -9,47 +9,49 @@ import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class GameEventRecorder implements InitializingBean {
-	
-	
 
 	Logger log = LoggerFactory.getLogger(getClass());
-	
+
 	protected JdbcTemplate jdbc;
 
-
 	@Resource
-    public void setDataSource(DataSource dataSource) {
+	public void setDataSource(DataSource dataSource) {
 		log.info("Setting datasource and creating jdbcTemplate");
-        this.jdbc = new JdbcTemplate(dataSource);
-    }
-	
-	
+		this.jdbc = new JdbcTemplate(dataSource);
+	}
 
 	protected void setupStorage() {
 		log.info("Creating table for load_testing_events");
-		jdbc.execute("create table if not exists load_testing_events (" +
-				"id int unsigned NOT NULL AUTO_INCREMENT," +
-				"user_id int unsigned not null," +
-				"log_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP," +
-				"event_type int unsigned not null," +
-				"event_bytes blob not null," +
-				"PRIMARY KEY (id))");
+		jdbc.execute("create table if not exists load_testing_events ("
+				+ "id int unsigned NOT NULL AUTO_INCREMENT,"
+				+ "user_id int unsigned not null,"
+				+ "log_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,"
+				+ "event_type int unsigned not null,"
+				+ "event_bytes blob not null," + "PRIMARY KEY (id))");
 	}
-	
+
 	@Override
 	public void afterPropertiesSet() throws Exception {
 		setupStorage();
 	}
-	
-	
-	public void persistEvent(Integer userId, Integer eventType, byte[] eventBytes) {
-		jdbc.update("insert into load_testing_events (user_id, log_time, event_type, event_bytes) values (?,?,?,?)",
-				userId,new Timestamp(new Date().getTime()),eventType,eventBytes);
+
+	public void persistEvent(Integer userId, Integer eventType,	byte[] eventBytes) {
+		if (userId != null && eventBytes != null) {
+			try {
+				log.info("Persisting event for user: {}", userId);
+				jdbc.update(
+						"insert into load_testing_events (user_id, log_time, event_type, event_bytes) values (?,?,?,?)",
+						userId, 
+						new Timestamp(new Date().getTime()), 
+						eventType,
+						eventBytes);
+			} catch (Exception e) {
+				log.error("Error persisting event:", e);
+			}
+		}
 	}
-	
-	
+
 }
