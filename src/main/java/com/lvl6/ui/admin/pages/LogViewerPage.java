5f614a14615385876760ae37c167f20c555aca4c
Kelly Roman <kelly@dekayd.com>
2012-08-24 17:07:27 -0700

    Hooking up wicket pages for log search

--- src/main/java/com/lvl6/ui/admin/pages/LogViewerPage.java
+++ src/main/java/com/lvl6/ui/admin/pages/LogViewerPage.java
@@ -8,14 +8,9 @@ import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.lvl6.cassandra.log4j.Log4jElasticSearchQuery;
-import com.lvl6.properties.Globals;
 import com.lvl6.spring.AppContext;
 import com.lvl6.ui.admin.components.LogSearchForm;
 import com.lvl6.ui.admin.components.LogSearchInputModel;
-import com.lvl6.ui.admin.components.RecentPurchasesPanel;
-import com.lvl6.ui.admin.components.ReloadStaticDataLink;
-import com.lvl6.ui.admin.components.StatsPanel;
-import com.lvl6.ui.admin.components.TopSpendersPanel;
 
 public class LogViewerPage extends TemplatePage {
 
@@ -26,11 +21,11 @@ public class LogViewerPage extends TemplatePage {
 		super();
 		if (SecurityContextHolder.getContext() != null && SecurityContextHolder.getContext().getAuthentication() != null) {
 			String user = SecurityContextHolder.getContext().getAuthentication().getName();
-			log.info("Loading Admin Page for: {}", user);
+			log.info("Loading Log Viewer Page for: {}", user);
 		} else {
-			log.info("Loading Admin Page");
+			log.info("Loading Log Viewer Page");
 		}
-
+		setup();
 	}
 	
 	protected SearchResponse result;
@@ -39,6 +34,7 @@ public class LogViewerPage extends TemplatePage {
 	protected void setup() {
 		form.setOutputMarkupId(true);
 		resultLabel.setOutputMarkupId(true);
+		feedback.setOutputMarkupId(true);
 		add(form);
 		add(resultLabel);
 		add(feedback);
@@ -53,7 +49,6 @@ public class LogViewerPage extends TemplatePage {
 
 		@Override
 		protected void onSubmit() {
-			// TODO Auto-generated method stub
 			super.onSubmit();
 			LogSearchInputModel model = getModelObject();
 			Log4jElasticSearchQuery search = AppContext.getApplicationContext().getBean(Log4jElasticSearchQuery.class);
