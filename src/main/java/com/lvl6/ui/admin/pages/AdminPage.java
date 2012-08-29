ddeb2cef4b764d00a31743f0c28cb9b39f48b4a2
Kelly Roman <kelly@dekayd.com>
2012-08-29 14:26:36 -0700

    Changed admin page

--- src/main/java/com/lvl6/ui/admin/pages/AdminPage.java
+++ src/main/java/com/lvl6/ui/admin/pages/AdminPage.java
@@ -4,7 +4,6 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.model.PropertyModel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.context.SecurityContextHolder;
