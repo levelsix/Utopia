47a1d78580e93e33c8418169f3e86e5be78468b4
Kelly Roman <kelly@dekayd.com>
2012-09-04 16:31:58 -0700

    Added reload leaderboard link

--- src/main/java/com/lvl6/ui/admin/pages/AdminPage.java
+++ src/main/java/com/lvl6/ui/admin/pages/AdminPage.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 
 import com.lvl6.properties.Globals;
 import com.lvl6.ui.admin.components.RecentPurchasesPanel;
+import com.lvl6.ui.admin.components.ReloadLeaderboardLink;
 import com.lvl6.ui.admin.components.ReloadStaticDataLink;
 import com.lvl6.ui.admin.components.StatsPanel;
 import com.lvl6.ui.admin.components.TopSpendersPanel;
@@ -38,6 +39,7 @@ public class AdminPage extends TemplatePage {
 	
 	private void setTools() {
 		add(new ReloadStaticDataLink("reloadStaticDataLink"));
+		add(new ReloadLeaderboardLink("reloadLeaderboardLink"));
 	}
 
 	protected void setStats() {
