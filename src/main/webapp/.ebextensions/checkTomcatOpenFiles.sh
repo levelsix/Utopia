4ea7c9ccb3bd7dc88a7763a2653bd95f0fd6ffb3
Kelly Roman <kelly@dekayd.com>
2012-10-15 05:40:49 -0700

    fixing tomcat opefn files check

--- src/main/webapp/.ebextensions/checkTomcatOpenFiles.sh
+++ src/main/webapp/.ebextensions/checkTomcatOpenFiles.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-OPENFILES=`sudo lsof | grep tomcat | wc -l`
+OPENFILES=`/usr/sbin/lsof | grep tomcat | wc -l`
 WHENTORESTART=$RANDOM
 RANGE=500
 let "WHENTORESTART %= $RANGE"
@@ -8,7 +8,7 @@ WHENTORESTART=$(($WHENTORESTART + 3000))
 if [ "$OPENFILES" -gt "$WHENTORESTART" ]
 then
 	echo Tomcats open files $OPENFILES is greater than $WHENTORESTART so restarting
-	sudo /etc/init.d/tomcat7 restart
+	/etc/init.d/tomcat7 restart
 else
 		echo Tomcats open files $OPENFILES is not greater than $WHENTORESTART so not restarting
 fi
