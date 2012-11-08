e012f54a003930429855522c71c2e1dd1ba5bae4
Ashwin Kamath <ashwin@lvl6.com>
2012-11-08 01:23:48 -0800

    added notification that we are awarding everyone 50 gold

--- src/main/java/com/lvl6/properties/ControllerConstants.java
+++ src/main/java/com/lvl6/properties/ControllerConstants.java
@@ -397,6 +397,7 @@ public class ControllerConstants {
   public static final String[] STARTUP__NOTICES_TO_PLAYERS = {
 //    "FREE limited edition gold equip for joining today!"
 //    "Forging Contest! 50 GOLD reward! Details at forum.lvl6.com"
+      "We have awarded everyone 50 gold for your patience with the bugs!"
     };
   
   
