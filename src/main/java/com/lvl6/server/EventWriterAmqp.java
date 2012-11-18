955447313e3d58aabfd4df945b17898fda2ea4de
bluestix <endgame2013@gmail.com>
2012-11-17 22:01:34 -0700

    Got tests to pass with refactored event writer

--- src/main/java/com/lvl6/server/EventWriterAmqp.java
+++ src/main/java/com/lvl6/server/EventWriterAmqp.java
@@ -2,6 +2,8 @@ package com.lvl6.server;
 
 import java.nio.ByteBuffer;
 
+import javax.annotation.Resource;
+
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.MessageProperties;
@@ -15,14 +17,12 @@ import com.lvl6.events.ResponseEvent;
 import com.lvl6.properties.Globals;
 import com.lvl6.retrieveutils.UserClanRetrieveUtils;
 import com.lvl6.utils.NIOUtils;
-import com.lvl6.utils.Wrap;
 
-public class EventWriterAmqp extends Wrap {
+public class EventWriterAmqp extends EventWriter {
 
-	@Autowired
-	RabbitTemplate clientsTemplate;
-	
-	
+	@Resource(name = "clientMessagesTemplate")
+	RabbitTemplate	clientsTemplate;
+
 	public RabbitTemplate getClientsTemplate() {
 		return clientsTemplate;
 	}
@@ -32,7 +32,7 @@ public class EventWriterAmqp extends Wrap {
 	}
 
 	@Autowired
-	UserClanRetrieveUtils userClanRetrieveUtil;
+	UserClanRetrieveUtils	userClanRetrieveUtil;
 
 	public UserClanRetrieveUtils getUserClanRetrieveUtil() {
 		return userClanRetrieveUtil;
@@ -42,12 +42,20 @@ public class EventWriterAmqp extends Wrap {
 		this.userClanRetrieveUtil = userClanRetrieveUtil;
 	}
 
-	private static org.slf4j.Logger log = LoggerFactory.getLogger(EventWriter.class);
+	private static org.slf4j.Logger	log	= LoggerFactory.getLogger(EventWriterAmqp.class);
 
 	protected void processEvent(GameEvent event) {
 		if (event instanceof ResponseEvent)
 			processResponseEvent((ResponseEvent) event);
 
+	}
+
+	public void processPreDBResponseEvent(ResponseEvent event, String udid) {
+		ByteBuffer buff = getBytes(event);
+		MessageProperties msgProps = new MessageProperties();
+		log.info("writing predb event with type=" + event.getEventType() + " to player with udid " + udid
+				+ ", event=" + event);
+		clientsTemplate.send(udid, new Message(buff.duplicate().array(), msgProps));
 	}
 
 	/**
@@ -62,10 +70,8 @@ public class EventWriterAmqp extends Wrap {
 			int[] recipients = ((BroadcastResponseEvent) event).getRecipients();
 			for (int i = 0; i < recipients.length; i++) {
 				if (recipients[i] > 0) {
-					log.info("writing broadcast event with type="
-							+ event.getEventType() 
-							+ " to players with ids "
-							+ recipients[i]);
+					log.info("writing broadcast event with type=" + event.getEventType()
+							+ " to players with ids " + recipients[i]);
 					sendMessageToPlayer(buff, msgProps, recipients[i]);
 				}
 			}
@@ -73,38 +79,33 @@ public class EventWriterAmqp extends Wrap {
 		// Otherwise this is just a normal message, send response to sender.
 		else {
 			int playerId = ((NormalResponseEvent) event).getPlayerId();
-			log.info("writing normal event with type="
-					+ event.getEventType() 
-					+ " to player with id "
-					+ playerId 
-					+ ", event=" 
-					+ event);
+			log.info("writing normal event with type=" + event.getEventType() + " to player with id "
+					+ playerId + ", event=" + event);
 			sendMessageToPlayer(buff, msgProps, playerId);
 		}
 	}
 
-	
-	protected void sendMessageToPlayer(ByteBuffer buff,	MessageProperties msgProps, int playerId) {
-		clientsTemplate.send(""+playerId, new Message(buff.duplicate().array(), msgProps));
+	protected void sendMessageToPlayer(ByteBuffer buff, MessageProperties msgProps, int playerId) {
+		clientsTemplate.send("" + playerId, new Message(buff.duplicate().array(), msgProps));
 	}
 
 	public void processClanResponseEvent(GameEvent event, int clanId) {
-/*		log.debug("writer received clan event=" + event);
-		ResponseEvent e = (ResponseEvent) event;
-		ByteBuffer buff = getBytes(e);
-		List<UserClan> playersInClan = userClanRetrieveUtil.getUserClanMembersInClan(clanId);
-		for (UserClan uc : playersInClan) {
-			log.info("Sending response to clan: {}  member: {}",uc.getClanId(),uc.getUserId());
-			//sendMessageToPlayer(e, buff.duplicate(), uc.getUserId());
-		}*/
+		/*
+		 * log.debug("writer received clan event=" + event); ResponseEvent e =
+		 * (ResponseEvent) event; ByteBuffer buff = getBytes(e); List<UserClan>
+		 * playersInClan =
+		 * userClanRetrieveUtil.getUserClanMembersInClan(clanId); for (UserClan
+		 * uc : playersInClan) {
+		 * log.info("Sending response to clan: {}  member: {}"
+		 * ,uc.getClanId(),uc.getUserId()); //sendMessageToPlayer(e,
+		 * buff.duplicate(), uc.getUserId()); }
+		 */
 	}
 
 	protected ByteBuffer getBytes(ResponseEvent event) {
-		ByteBuffer writeBuffer = ByteBuffer	.allocateDirect(Globals.MAX_EVENT_SIZE);
+		ByteBuffer writeBuffer = ByteBuffer.allocateDirect(Globals.MAX_EVENT_SIZE);
 		NIOUtils.prepBuffer(event, writeBuffer);
 		return writeBuffer;
 	}
-
-
 
 }// EventWriter
