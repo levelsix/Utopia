60fb92d2b993bcf96d152b2eb4697592f4c6dfe6
Ashwin Kamath <ashwin@lvl6.com>
2012-12-11 23:54:45 -0800

    small fixes

--- src/main/java/com/lvl6/misc/MiscMethods.java
+++ src/main/java/com/lvl6/misc/MiscMethods.java
@@ -857,8 +857,7 @@ public class MiscMethods {
       ChangedClanTowerResponseEvent e = new ChangedClanTowerResponseEvent(0);
       e.setChangedClanTowerResponseProto(t.build());
       
-      MessagingUtil msgUtil = AppContext.getApplicationContext().getBean(MessagingUtil.class);
-      msgUtil.sendGlobalMessage(e);
+      server.writeGlobalEvent(e);
     }
   }
   
