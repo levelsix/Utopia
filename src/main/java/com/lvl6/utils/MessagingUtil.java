9d165cb25f3a19c5013be9276dfe8ba8a66dd9c1
bluestix <endgame2013@gmail.com>
2013-01-14 21:12:31 -0700

    Fixes for maintenance mode

--- src/main/java/com/lvl6/utils/MessagingUtil.java
+++ src/main/java/com/lvl6/utils/MessagingUtil.java
@@ -55,15 +55,15 @@ public class MessagingUtil {
 	}
 	
 	
-	public void sendMaintanenceModeMessage(String message, int userId) {
+	public void sendMaintanenceModeMessage(String message, String udid) {
 		log.info("Sending maintenance mode message: \"{}\" to player {}", message, userId);
 		//send admin message
 		SendAdminMessageResponseProto.Builder samrp = SendAdminMessageResponseProto.newBuilder();
 		samrp.setMessage(message);
 		samrp.setSenderId(ControllerConstants.USER_CREATE__ID_OF_POSTER_OF_FIRST_WALL);
-		SendAdminMessageResponseEvent ev = new SendAdminMessageResponseEvent(userId);
+		SendAdminMessageResponseEvent ev = new SendAdminMessageResponseEvent(-1);
 		ev.setSendAdminMessageResponseProto(samrp.build());
-		eventWriter.handleEvent(ev);
+		eventWriter.processPreDBResponseEvent(ev, udid);
 	}
 	
 	public void sendAdminMessage(String message) {
