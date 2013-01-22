e110ae83d9ed4168dda7a48d19de6b744cc3555b
bluestix <endgame2013@gmail.com>
2013-01-22 09:51:09 -0700

    Changing lock logging to info

--- src/main/java/com/lvl6/server/GameServer.java
+++ src/main/java/com/lvl6/server/GameServer.java
@@ -393,7 +393,7 @@ public class GameServer implements InitializingBean, HazelcastInstanceAware {
 	}
 
 	public void unlockPlayer(int playerId) {
-		log.debug("Unlocking player: " + playerId);
+		log.info("Unlocking player: " + playerId);
 		// ILock lock = hazel.getLock(playersInAction.lockName(playerId));
 		try {
 			if (lockMap.isLocked(playersInAction.lockName(playerId))) {
