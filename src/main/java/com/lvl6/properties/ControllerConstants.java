5ebd28e20965464b716e4b7245dd4ccaf1634742
Ashwin Kamath <ashwin@lvl6.com>
2013-01-23 18:11:33 -0800

    small change to min clan members

--- src/main/java/com/lvl6/properties/ControllerConstants.java
+++ src/main/java/com/lvl6/properties/ControllerConstants.java
@@ -395,7 +395,7 @@ public class ControllerConstants {
   public static final int LEVEL_TO_SHOW_RATE_US_POPUP = 8;
   
   //CLAN TOWER
-  public static final int MIN_CLAN_MEMBERS_TO_HOLD_CLAN_TOWER = 25;
+  public static final int MIN_CLAN_MEMBERS_TO_HOLD_CLAN_TOWER = 2;
   
   //LEADERBOARD EVENT
   public static final int LEADERBOARD_EVENT__WINS_WEIGHT = 2;
