da63c3e9dd099b7c0fa1f29da036ffe31eecc972
Ashwin Kamath <ashwin@lvl6.com>
2013-02-01 04:52:09 -0800

    log

--- src/main/java/com/lvl6/misc/MiscMethods.java
+++ src/main/java/com/lvl6/misc/MiscMethods.java
@@ -1368,7 +1368,7 @@ public class MiscMethods {
     int d = winner.getLevel()-loser.getLevel();
     int pts = (int)Math.round((-0.0006*Math.pow(d, 5)+0.0601*Math.pow(d, 4)-0.779*Math.pow(d, 3)
         +2.4946*Math.pow(d, 2)-9.7046*d+89.905)/10.);
-    
+    log.info(pts+"");
     return Math.min(100, Math.max(1, pts));
   }
 }
