d3cc00eec9a629ed095345b97e52116be9153621
Ashwin Kamath <ashwin@lvl6.com>
2013-03-12 21:24:27 -0700

    changed info img name

--- src/main/java/com/lvl6/properties/ControllerConstants.java
+++ src/main/java/com/lvl6/properties/ControllerConstants.java
@@ -525,7 +525,7 @@ public class ControllerConstants {
   //amount of booster packs user can buy at one time
   public static final int BOOSTER_PACK__PURCHASE_OPTION_ONE_NUM_BOOSTER_ITEMS = 1;
   public static final int BOOSTER_PACK__PURCHASE_OPTION_TWO_NUM_BOOSTER_ITEMS = 10;
-  public static final String BOOSTER_PACK__INFO_IMAGE_NAME = "insertstringhere";
+  public static final String BOOSTER_PACK__INFO_IMAGE_NAME = "howchestswork.png";
   public static final int BOOSTER_PACK__NUM_TIMES_TO_BUY_STARTER_PACK = 4;
   public static final int BOOSTER_PACK__NUM_DAYS_TO_BUY_STARTER_PACK = 3;
   
