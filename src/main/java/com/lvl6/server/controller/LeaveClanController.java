c744b059e9b3c4d054b18dd64da31743c819c394
Arturo Wu Zhou <aweezy90@gmail.com>
2013-04-02 15:23:37 -0700

    commented out leaving clan notification on grounds that deserter still sees the message that he left when he joins a clan / reenters bazaar

--- src/main/java/com/lvl6/server/controller/LeaveClanController.java
+++ src/main/java/com/lvl6/server/controller/LeaveClanController.java
@@ -100,7 +100,9 @@ import com.lvl6.utils.utilmethods.DeleteUtils;
         resEventUpdate.setTag(event.getTag());
         server.writeEvent(resEventUpdate);
         
-        notifyClan(user, clan);
+        //this works for other clan members, but not for the person 
+        //who left (they see the message when they join a clan, reenter clan house
+        //notifyClan(user, clan);
         
         //clan tower stuff
         if(server.lockClanTowersTable()) {
