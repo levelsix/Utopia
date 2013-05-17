fa6dbf96de8d4ec5cda5022139477114f825333a
bluestix <endgame2013@gmail.com>
2013-05-16 23:23:24 -0700

    Fixing bug in AdminChatPage

--- src/main/java/com/lvl6/utils/AdminChatUtil.java
+++ src/main/java/com/lvl6/utils/AdminChatUtil.java
@@ -100,6 +100,9 @@ public class AdminChatUtil {
 	}
 
 	protected List<AdminChatPost> addUsernames(List<AdminChatPost> msgs) {
+		if(msgs.size() == 0) {
+			return msgs;
+		}
 		List<Integer> users = new ArrayList<Integer>();
 		for (AdminChatPost msg : msgs) {
 			if (msg.getPosterId() != ControllerConstants.STARTUP__ADMIN_CHAT_USER_ID) {
