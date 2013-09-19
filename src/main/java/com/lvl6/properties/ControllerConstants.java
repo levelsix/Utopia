a27ce0c2a0afdc6a70507b39712ab895f5b8da65
Arturo Wu Zhou <aweezy90@gmail.com>
2013-09-18 19:11:35 -0700

    commenting out Globals.KABAM_ENABLED()

--- src/main/java/com/lvl6/properties/ControllerConstants.java
+++ src/main/java/com/lvl6/properties/ControllerConstants.java
@@ -519,7 +519,7 @@ public class ControllerConstants {
   
   //CLAN TOWER
   //on production, aoc 25 minimum to hold or attack a tower, but on loc 20
-  public static final int MIN_CLAN_MEMBERS_TO_HOLD_CLAN_TOWER = Globals.IS_SANDBOX() ? 2 : (Globals.KABAM_ENABLED() ? 25 : 20);
+  public static final int MIN_CLAN_MEMBERS_TO_HOLD_CLAN_TOWER = Globals.IS_SANDBOX() ? 2 : 25;//(Globals.KABAM_ENABLED() ? 25 : 20);
   public static final int NUM_HOURS_BEFORE_REWAGING_WAR_ON_TOWER = 6;
   public static final int CLAN_TOWER__MAX_NUM_TOWERS_CLAN_CAN_HOLD = 1;
   
